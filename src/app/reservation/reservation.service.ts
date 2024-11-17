6a9056ea73d791bad00bdfd30b2573b9217f1611
jahicm <mirzajahic1@hotmail.com>
2024-11-17 17:29:48 +0100

    Commit 1 clean up

---
+++ src/app/reservation/reservation.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+
+  private reservations: Reservation[]=[];
+
+
+  
+  getReservations():Reservation[]
+  {
+    return this.reservations;
+  }
+  getReservation(id:string): Reservation | undefined
+  {
+      return this.reservations.find(res=>res.id==id);
+  }
+  addReservation(reservation: Reservation):void
+  {
+      reservation.id = Date.now().toString();
+      this.reservations.push(reservation);
+      
+  }
+  deleteReservation(id: string)
+  {
+    let index = this.reservations.findIndex(res=>res.id==id);
+    this.reservations.splice(index,1);
+  }
+  updateReservations(id:string,updatedReservation: Reservation):void
+  {
+    let index  = this.reservations.findIndex(res=>res.id ==id);
+    this.reservations[index] = updatedReservation;
+  }
+}
